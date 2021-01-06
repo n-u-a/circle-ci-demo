387f9364a5c8d3af9d289f5a4d288f9c2602e8c0
n-u-a <freizeit.g@gmail.com>
2021-01-06 23:54:58 +0000

    modify to get expected result

--- src/main/java/com/example/demopro/controller/IndexController.java
+++ src/main/java/com/example/demopro/controller/IndexController.java
@@ -16,7 +16,7 @@ public class IndexController {
     @RequestMapping({ "/", "/index" })
     public ModelAndView get(ModelAndView mav) {
         mav.addObject("names", service.getAll());
-        mav.setViewName("circle-ci-share");
+        mav.setViewName("index");
         return mav;
     }
 }
