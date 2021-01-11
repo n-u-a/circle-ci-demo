3aa7b6ff0e6eddda526ec5231050700fab92722b
n-u-a <freizeit.g@gmail.com>
2021-01-11 06:30:32 +0000

    add inheritance to auto deploy

--- src/main/java/com/example/demopro/DemoproApplication.java
+++ src/main/java/com/example/demopro/DemoproApplication.java
@@ -2,9 +2,16 @@ package com.example.demopro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoproApplication {
+public class DemoproApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoproApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoproApplication.class, args);
