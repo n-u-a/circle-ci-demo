11cad5c558bb7af2bd9b4966e5cdf7b030296c6f
n-u-a <freizeit.g@gmail.com>
2021-03-07 05:07:56 +0000

    remove

--- src/main/java/com/example/demopro/controller/IndexController.java
+++ src/main/java/com/example/demopro/controller/IndexController.java
@@ -1,17 +1,11 @@
 package com.example.demopro.controller;
 
-import com.example.demopro.service.NameService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
-
-    @Autowired
-    private NameService service;
 
     @RequestMapping({ "/", "/index" })
     public ModelAndView get(ModelAndView mav) {
